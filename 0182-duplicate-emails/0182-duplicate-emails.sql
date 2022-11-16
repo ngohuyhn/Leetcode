bfd8ed34d89b25397c1d5a34711ea9e8d98d5a1f
Ngo Quang Huy <115140527+ngohuyhn@users.noreply.github.com>
2022-11-16 14:14:31 +0700

    Update 0182-duplicate-emails.sql

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,4 +1,4 @@
-/* Write your T-SQL query statement below */
+/* Write your T-SQL query statement below */ -- Group colume Email rồi count những địa chỉ mail xuất hiện nhiều hơn 1 lần
 select pe.email
 from Person as Pe 
 group by pe.email 
