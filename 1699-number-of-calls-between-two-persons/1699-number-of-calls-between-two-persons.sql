1f3da45846788ebef330dce2aac2c5fdf9ee43df
Ngo Quang Huy <115140527+ngohuyhn@users.noreply.github.com>
2022-11-21 16:57:10 +0700

    Time: 2784 ms (17.41%), Space: 0B (100.00%) - LeetHub

--- 1699-number-of-calls-between-two-persons/1699-number-of-calls-between-two-persons.sql
+++ 1699-number-of-calls-between-two-persons/1699-number-of-calls-between-two-persons.sql
@@ -1,9 +1,25 @@
 /* Write your T-SQL query statement below */
-select case when from_id < to_id then from_id else to_id end as person1,
+/* select 
+case when from_id < to_id then from_id else to_id end as person1,
 case when from_id > to_id then from_id else to_id end as person2,
 count(*) as call_count,
 SUM(duration) as total_duration
 from calls
 group by 
 case when from_id < to_id then from_id else to_id end,
-case when from_id > to_id then from_id else to_id end 
+case when from_id > to_id then from_id else to_id end  */
+
+SELECT
+	CASE WHEN from_id < to_id THEN from_id
+	ELSE to_id END AS person1,
+	CASE WHEN from_id > to_id THEN from_id
+	ELSE to_id END as person2,
+	COUNT(*) AS call_count,
+	SUM(duration) AS total_duration
+FROM
+    Calls
+GROUP BY
+    CASE WHEN from_id < to_id THEN from_id
+	ELSE to_id END,
+    CASE WHEN from_id > to_id THEN from_id
+	ELSE to_id END
